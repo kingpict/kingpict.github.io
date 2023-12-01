e0185dcc292f0ac7b270d9b233ac215f50c97702
kingpict <151995945+kingpict@users.noreply.github.com>
2023-12-01 19:17:54 +0700

    download root.bat (link)

---
+++ download/root.bat
@@ -0,0 +1,27 @@
+@echo off
+title ROOT .... LIEUANNHIEU SOFTWARE 
+echo ROOT ....: AN NUT BAT KY DE BI AN LINH TINH MAY
+pause
+cmd.exe "echo BAN SE BI TRUY CAP NHUNG WEBSITE NGUY HIEM NEU BAN ENTER"
+cmd.exe"echo BAN SE BI TRUY CAP NHUNG WEBSITE NGUY HIEM NEU BAN ENTER"
+cmd.exe "echo BAN SE BI TRUY CAP NHUNG WEBSITE NGUY HIEM NEU BAN ENTER"
+cmd.exe "echo BAN SE BI TRUY CAP NHUNG WEBSITE NGUY HIEM NEU BAN ENTER"
+cmd.exe "echo BAN SE BI TRUY CAP NHUNG WEBSITE NGUY HIEM NEU BAN ENTER"
+cmd.exe "echo BAN SE BI TRUY CAP NHUNG WEBSITE NGUY HIEM NEU BAN ENTER"
+pause
+taskmgr "cmd.exe"
+taskmgr "cmd.exe"
+taskmgr "C:/"
+taskmgr "D:/"
+taskmgr "C:/Windows/system32"
+taskmgr "C:/Windows" "cmd.exe""cd .."
+taskmgr
+coccoc.exe "takethislollipop.com"
+chrome.exe "takethislollipop.com"
+coccoc.exe "Whiteenamel.com"
+chrome.exe "Whiteenamel.com"
+chrome.exe "opentopia.com"
+coccoc.exe "opentopia.com"
+firefox.exe "takethislollipop.com"
+firefox.com "whiteenamel.com"
+firefox.com "opentopia.com"
